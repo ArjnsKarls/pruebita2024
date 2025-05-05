28bbdac6212d205e70bcd4b8ae3420df9c2de71f
ArjnsKarl <carimerma@gmail.com>
2025-05-05 02:20:34 -0400

    agregando2

--- secrets.py
+++ secrets.py
@@ -1,5 +1,9 @@
-API_KEY = "AKIAIOSFODNN7EXAMPLE"
+[default]
 
-SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCMEKE"
+aws_access_key_id = AKIAQYLPMN5HHHFPZAM2
 
-password = "1234SuperSecreto"
+aws_secret_access_key = 1tUm636uS1yOEcfP5pvfq/ml36m7kys0U
+
+output = json
+
+region = us-east-2
